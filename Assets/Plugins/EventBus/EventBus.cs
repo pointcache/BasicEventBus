41e0efdbfdd5374639bac3e3de37203d1b83c770
pointcache <antonov.3d@gmail.com>
2023-06-29 02:11:50 +0300

    updated a bit

---
+++ Assets/Plugins/EventBus/EventBus.cs
@@ -0,0 +1,255 @@
+namespace EventBus
+{
+    using System;
+    using System.Reflection;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using EventBus.Internal;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+    using System.Collections;
+#endif
+
+    public static class EventBusUtility
+    {
+        public static IReadOnlyList<Type> EventTypes { get; private set; }
+        public static IReadOnlyList<Type> StaticEventBusesTypes { get; private set; }
+
+#if UNITY_EDITOR
+        public static PlayModeStateChange PlayModeState { get; private set; }
+
+        [InitializeOnLoadMethod]
+        public static void InitializeEditor()
+        {
+            EditorApplication.playModeStateChanged -= HandleEditorStateChange;
+            EditorApplication.playModeStateChanged += HandleEditorStateChange;
+        }
+
+        private static void HandleEditorStateChange(PlayModeStateChange state)
+        {
+            PlayModeState = state;
+
+            if (PlayModeState == PlayModeStateChange.EnteredEditMode)
+                ClearAllBuses();
+        }
+#endif
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Init()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Type[] assemblyCSharp = null;
+            Type[] assemblyCSharpFirstpass = null;
+
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                if (assemblies[i].GetName().Name == "Assembly-CSharp")
+                    assemblyCSharp = assemblies[i].GetTypes();
+                else
+                    if (assemblies[i].GetName().Name == "Assembly-CSharp-firstpass")
+                    assemblyCSharpFirstpass = assemblies[i].GetTypes();
+
+                if (assemblyCSharp != null && assemblyCSharpFirstpass != null)
+                    break;
+            }
+
+            List<Type> eventTypes = new List<Type>();
+
+            for (int i = 0; i < assemblyCSharp.Length; i++)
+            {
+                var type = assemblyCSharp.GetType();
+                if ((typeof(IEvent)) != type && (typeof(IEvent)).IsAssignableFrom(type))
+                {
+                    eventTypes.Add(type);
+                }
+            }
+
+            for (int i = 0; i < assemblyCSharpFirstpass.Length; i++)
+            {
+                var type = assemblyCSharpFirstpass.GetType();
+                if ((typeof(IEvent)) != type && (typeof(IEvent)).IsAssignableFrom(type))
+                {
+                    eventTypes.Add(type);
+                }
+            }
+
+            EventTypes = eventTypes;
+
+            List<Type> staticEventBusesTypes = new List<Type>();
+            var typedef = typeof(EventBus<>);
+            for (int i = 0; i < EventTypes.Count; i++)
+            {
+                var type = EventTypes[i];
+                var gentype = typedef.MakeGenericType(type);
+                staticEventBusesTypes.Add(gentype);
+            }
+
+            StaticEventBusesTypes = staticEventBusesTypes;
+        }
+
+        public static void ClearAllBuses()
+        {
+            for (int i = 0; i < StaticEventBusesTypes.Count; i++)
+            {
+                var type = EventTypes[i];
+                var clearMethod = type.GetMethod("Clear", BindingFlags.Static | BindingFlags.NonPublic);
+                clearMethod.Invoke(null, null);
+            }
+        }
+    }
+
+    public static class EventBus<T> where T : struct, IEvent
+    {
+        private static EventBinding<T>[] bindings = new EventBinding<T>[64];
+        private static List<Callback> callbacks = new List<Callback>();
+        private static int count;
+
+        public class Awaiter : EventBinding<T>
+        {
+            public bool EventRaised { get; private set; }
+            public T Payload { get; private set; }
+
+            public Awaiter() : base((Action)null)
+            {
+                ((IEventBindingInternal<T>)this).OnEvent = OnEvent;
+            }
+
+            private void OnEvent(T ev)
+            {
+                EventRaised = true;
+                Payload = ev;
+            }
+        }
+
+        private struct Callback
+        {
+            public Action onEventNoArg;
+            public Action<T> onEvent;
+        }
+
+        private static void Clear()
+        {
+            bindings = new EventBinding<T>[64];
+            callbacks.Clear();
+            count = 0;
+        }
+
+        public static void Register(EventBinding<T> binding)
+        {
+            if (binding.Registered)
+                return;
+
+            if (bindings.Length <= count)
+            {
+                EventBinding<T>[] newarray = new EventBinding<T>[bindings.Length * 2];
+                Array.Copy(bindings, newarray, bindings.Length);
+                bindings = newarray;
+            }
+
+            binding.InternalIndex = count;
+            bindings[count] = binding;
+
+            count++;
+        }
+
+        public static void AddCallback(Action callback)
+        {
+            if (callback == null)
+                return;
+            callbacks.Add(new Callback() { onEventNoArg = callback });
+        }
+
+        public static void AddCallback(Action<T> callback)
+        {
+            if (callback == null)
+                return;
+            callbacks.Add(new Callback() { onEvent = callback });
+        }
+
+        public static void Unregister(EventBinding<T> binding)
+        {
+#if UNITY_EDITOR
+            if (EventBusUtility.PlayModeState == PlayModeStateChange.ExitingPlayMode)
+                return;
+#endif
+            int index = binding.InternalIndex;
+
+            if (index == -1 || index > count)
+            {
+                // binding invalid
+                return;
+            }
+
+            if (bindings[index] != binding)
+            {
+                // binding invalid
+                return;
+            }
+
+            if (index == 0)
+            {
+                bindings[0] = null;
+                binding.InternalIndex = -1;
+                count--;
+                return;
+            }
+
+            int lastIndex = count - 1;
+            var last = bindings[lastIndex];
+
+            bindings[index] = last;
+            bindings[lastIndex] = null;
+
+            if (last != null)
+                last.InternalIndex = index;
+            binding.InternalIndex = -1;
+
+            count--;
+        }
+
+        public static void Raise()
+        {
+            Raise(default);
+        }
+
+        public static void Raise(T ev)
+        {
+#if UNITY_EDITOR
+            if (EventBusUtility.PlayModeState == PlayModeStateChange.ExitingPlayMode)
+                return;
+#endif
+            for (int i = 0; i < count; i++)
+            {
+                IEventBindingInternal<T> internalBind = bindings[i];
+                internalBind.OnEvent?.Invoke(ev);
+                internalBind.OnEventArgs?.Invoke();
+            }
+
+            for (int i = 0; i < callbacks.Count; i++)
+            {
+                callbacks[i].onEvent?.Invoke(ev);
+                callbacks[i].onEventNoArg?.Invoke();
+            }
+
+            callbacks.Clear();
+        }
+
+        public static string GetDebugInfoString()
+        {
+            return "Bindings: " + count + " BufferSize: " + bindings.Length + "\n"
+                + "Callbacks: " + callbacks.Count;
+        }
+
+        /// <summary>
+        /// Allocates an Awaiter : EventBinding<T>
+        /// Use to await event in coroutines
+        /// </summary>
+        /// <returns></returns>
+        public static Awaiter NewAwaiter()
+        {
+            // TODO: do it non alloc
+            return new Awaiter();
+        }
+    }
+}
---
+++ Assets/Plugins/EventBus/Example/EventBusExample.cs
@@ -0,0 +1,108 @@
+﻿namespace EventBus.Example
+{
+    using System.Collections;
+    using EventBus;
+    using MyGame.Events;
+    using UnityEngine;
+
+    namespace MyGame.Events
+    {
+        public struct TestEvent : IEvent
+        {
+            public string a;
+            public float b;
+        }
+
+        public struct EventThatWillBeAwaitedByCoroutine : IEvent
+        {
+            public string message;
+        }
+    }
+
+    namespace MyGame.Events.UI
+    {
+        public struct OnClickNewGame : IEvent { }
+        public struct OnWantsToDeleteInventoryItem : IEvent { }
+        public struct OnWaitingForPlayerInput : IEvent { }
+    }
+
+    public class EventBusExample : MonoBehaviour
+    {
+        public int PerFrame = 1000;
+        public bool testPerformance;
+
+        EventBinding<TestEvent> _onTestEvent;
+        float accumulate;
+
+        private void Awake()
+        {
+            _onTestEvent = new EventBinding<TestEvent>(OnEvent);
+
+            _onTestEvent.Add(onEventButPrintTheStringInstead);
+            _onTestEvent.Add(someUnrelatedMethodWithoutArgs);
+
+            EventBus<TestEvent>.Raise(new TestEvent()
+            {
+                a = "Hello"
+            });
+
+            _onTestEvent.Remove(onEventButPrintTheStringInstead);
+            _onTestEvent.Remove(someUnrelatedMethodWithoutArgs);
+
+            // Callback will be called precisely once, then dropped
+            EventBus<TestEvent>.AddCallback(SomeRandomMethod);
+
+            StartCoroutine(CoroutineThatDispatchesEventAfterSomeTime());
+            StartCoroutine(CoroutineThatWaitsForEvent());
+        }
+
+        private IEnumerator CoroutineThatDispatchesEventAfterSomeTime()
+        {
+            yield return new WaitForSeconds(3);
+            EventBus<EventThatWillBeAwaitedByCoroutine>.Raise(new EventThatWillBeAwaitedByCoroutine() { message = "Say yay!" });
+        }
+
+        private IEnumerator CoroutineThatWaitsForEvent()
+        {
+            var awaiter = EventBus<EventThatWillBeAwaitedByCoroutine>.NewAwaiter();
+            while (!awaiter.EventRaised)
+                yield return null;
+            Debug.Log(awaiter.Payload.message);
+            Debug.Log("Yay!");
+        }
+
+        private void OnEnable()
+        {
+            _onTestEvent.Listen = true;
+        }
+
+        private void OnDisable()
+        {
+            _onTestEvent.Listen = false;
+        }
+
+        public void OnEvent(TestEvent e) => accumulate += e.b;
+
+        private void onEventButPrintTheStringInstead(TestEvent e) => Debug.Log(e.a);
+
+        private void someUnrelatedMethodWithoutArgs() => Debug.Log("Hello?");
+
+        private void SomeRandomMethod() => Debug.Log("I was called once");
+
+        void Update()
+        {
+            if (testPerformance)
+            {
+                for (int i = 0; i < PerFrame; i++)
+                {
+                    EventBus<TestEvent>.Raise(new TestEvent()
+                    {
+                        b = 7,
+                        a = "Hello"
+                    });
+                }
+            }
+        }
+    }
+
+}
